37ddfd2bec930e33484e80f2223a55054053535a
Andre Luiz D C <and969696@outlook.com>
2021-01-23 22:28:12 -0300

    all tests delivered

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -14,6 +14,15 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type is invade');
+    }
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new Error('You do not have enough balance');
+    }
+
     const transactions = this.transactionsRepository.create({
       title,
       value,
